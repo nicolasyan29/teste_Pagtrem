dbdd0b7e296683d8aa6b818b7ab69bdd428ebbf5
nicolasyan29 <nicolas_borba@estudante.sesisenai.org.br>
2025-11-18 12:05:41 -0300

    teste_1

---
+++ public/Tela_inicial.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>PagTrem Funcionários</title>
+    <link rel="stylesheet" href="../style/combined.css/">
+</head>
+<body>
+    <div class="container" onclick="iniciarApp()">
+        <div class="icon">
+            🚆
+        </div>
+        <h1>PagTrem</h1>
+        <h2>Funcionários</h2>
+        <p>Clique na tela para iniciar</p>
+    </div>
+
+    <script>
+        function iniciarApp() {
+            // Redireciona ou inicia lógica do app
+            alert("App iniciado!");
+        }
+    </script>
+</body>
+</html>
